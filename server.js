267c552dc98c7526886e8a430c2c47a9a97667a2
Bakos Heni <150692747+BakosHnei@users.noreply.github.com>
2025-02-17 14:31:55 +0100

    Select használata

--- server.js
+++ server.js
@@ -7,20 +7,37 @@ app.use(cors());
 app.use(bodyparser.json());
 
 const db = mysql.createConnection(
-{
-    user:"root",
-    host:"localhost",
-    port:3307,
-    password: "",
-    database: "teliolimpia"
-}
+    {
+        user: "root",
+        host: "localhost",
+        port: 3307,
+        password: "",
+        database: "teliolimpia"
+    }
 )
 //a szerver futásának ellenőrzése
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("A szerver működik!")
 }
 )
 
+app.get("/v", (req, res) => {
+    const sql = "SELECT * FROM versenyzok"
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    }
+    )
+})
+
+app.get("/v3",( req,res) => {
+        const sql = "select * from teliolimpia.versenyzok where ID = 3;"
+        db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    }
+    )
+})
 
 app.listen(3000, () => {
     console.log("A szerver a 3000 porton fut!")
